25a6c48eccbedf0d895b1d196f85eba219371884
DecafManiac <damianpdr@gmail.com>
2017-03-27 22:08:21 +0200

    few changes

--- js/script.js
+++ js/script.js
@@ -23,7 +23,44 @@ function navClickHandler(evt) {
     return false; 
 }
 
+function mouseEnterHandler(evt) {
+    // get data atrr values
+    
+    var title = $(evt.target).parent().data('title');
+    var description = $(evt.target).parent().data('desc');
+    // validation
+    
+    if (description == null) {
+        description = 'Click to enlarge';
+    
+    }
+    if(title == null) {
+        title = '';
+      
+    }
+    
+    //create overlay div\
+    $(evt.target).parent().parent().append('<div class="overlay"></div>');
+    // get overlay div
+    var overlay = $(evt.target).parent().parent().children('.overlay');
+    // add html to overlay
+    overlay.html('<h3>'+title+'</h3><p>'+description+'</p>');
+    // fade in overlay
+    overlay.fadeIn(800);
+}
+
+function mouseLeaveHandler(evt) {
+     //create overlay div\
+  
+    $(evt.target).parent().parent().append('<div class="overlay"></div>');
+    // get overlay div
+    var overlay = $(evt.target).parent().parent().children('.overlay');
+    // fade out
+    overlay.fadeOut(100);
+}
+
 $(document).ready(function() {
     $('nav a').on('click', navClickHandler);
-    
+    $('ul#gallery li').on('mouseenter', mouseEnterHandler)
+    $('ul#gallery li').on('mouseleave', mouseLeaveHandler)
 });
