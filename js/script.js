b53f58a8bbe535c2808a76d4629ba6b1323ef0c3
DecafManiac <damianpdr@gmail.com>
2017-03-28 19:10:00 +0200

    fix function "mouseLeave Handler"

--- js/script.js
+++ js/script.js
@@ -1,66 +1,72 @@
 
-function navClickHandler(evt) {
-    $('nav li.current').removeClass('current');
-    $(evt.target).parent().addClass('current');
-    // set heading 
-    $('#heading').text($(evt.target).text());
+let GalleryManager = (() => {
 
-    // Get and filter link text
-    let category = $(evt.target).text().toLowerCase().replace(' ', '-');
-    
-    //remove hidden addClass
-    if (category === 'all-projects') {
-        $('#gallery li:hidden').fadeIn('slow').removeClass('hidden');
-    } else {
-        $('#gallery li').each(function() {
-            if(!$(this).hasClass(category)) {
-                $(this).hide().addClass('hidden');
-            } else {
-                $(this).fadeIn('slow').removeClass('hidden');
-            }
+    jQuery.fn.fadeOutAndRemove = function(speed){
+        $(this).fadeOut(speed,function(){
+            $(this).remove();
         })
-    } //stop link behavior
-    return false; 
-}
-
-function mouseEnterHandler(evt) {
-    // get data atrr values
-    
-    let title = $(evt.target).parent().data('title');
-    let description = $(evt.target).parent().data('desc');
-    // validation
-    
-    if (description == null) {
-        description = 'Click to enlarge';
-    
     }
-    if(title == null) {
-        title = '';
-      
+
+    function navClickHandler(evt) {
+        $('nav li.current').removeClass('current');
+        $(evt.target).parent().addClass('current');
+        // set heading 
+        $('#heading').text($(evt.target).text());
+        // Get and filter link text
+        let category = $(evt.target).text().toLowerCase().replace(' ', '-');
+        //remove hidden addClass
+        if (category === 'all-projects') {
+            $('#gallery li:hidden').fadeIn('slow').removeClass('hidden');
+        } else {
+            $('#gallery li').each(function() {
+                if(!$(this).hasClass(category)) {
+                    $(this).hide().addClass('hidden');
+                } else {
+                    $(this).fadeIn('slow').removeClass('hidden');
+                }
+            })
+        } //stop link behavior
+        return false; 
+    }
+
+    function mouseEnterHandler(evt) {
+        // get data atrr values
+        let title = $(evt.currentTarget).children().data('title');
+        let description = $(evt.currentTarget).children().data('desc');
+        // validation
+        if (description == null) {
+            description = 'Click to enlarge';
+        }
+        if(title == null) {
+            title = '';
+        }
+        //create overlay div\
+        $(evt.currentTarget).append('<div class="overlay"></div>');
+        // get overlay div
+        let overlay = $(evt.currentTarget).children('.overlay');
+        // add html to overlay
+        overlay.html('<h3>'+title+'</h3><p>'+description+'</p>');
+        // fade in overlay
+        overlay.fadeIn(800);
+    }
+
+    function mouseLeaveHandler(evt) {
+        $(evt.currentTarget).children('.overlay').fadeOutAndRemove(400);
+    }
+   
+    function init() {
+        $('nav a').on('click', navClickHandler);
+        $('ul#gallery li').on('mouseenter', mouseEnterHandler);
+        $('ul#gallery li').on('mouseleave', mouseLeaveHandler);
     }
     
-    //create overlay div\
-    $(evt.target).parent().parent().append('<div class="overlay"></div>');
-    // get overlay div
-    let overlay = $(evt.target).parent().parent().children('.overlay');
-    // add html to overlay
-    overlay.html('<h3>'+title+'</h3><p>'+description+'</p>');
-    // fade in overlay
-    overlay.fadeIn(800);
-}
+    return {
+        init: init
+    }
+})();
 
-function mouseLeaveHandler(evt) {
-     //create overlay div\
-  
-    $(evt.target).parent().parent().append('<div class="overlay"></div>');
-    // get overlay div
-    let overlay = $(evt.target).parent().parent().children('.overlay');
-    // fade out
-    overlay.fadeOut(100);
-}
+
 
 $(document).ready(function() {
-    $('nav a').on('click', navClickHandler);
-    $('ul#gallery li').on('mouseenter', mouseEnterHandler)
-    $('ul#gallery li').on('mouseleave', mouseLeaveHandler)
+    GalleryManager.init();
 });
